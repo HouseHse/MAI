94ef84a4ad3961bec476331f4a6cf53341c474ba
H.se <mcmax1@yandex.ru>
2019-04-03 17:15:40 +0300

    Update kernel.cu

--- Laba2/kernel.cu
+++ Laba2/kernel.cu
@@ -7,7 +7,7 @@
 #include <clocale>
 
 #define BLOCK_SIZE 16
-/////Функция записи матрицы в файл
+/////Р¤СѓРЅРєС†РёСЏ Р·Р°РїРёСЃРё РјР°С‚СЂРёС†С‹ РІ С„Р°Р№Р»
 __host__ void SaveMatrixToFile(char* fileName, int* matrix, int width, int height) {
 	FILE* file = fopen(fileName, "wt");
 	for (int y = 0; y < height; y++) {
@@ -19,7 +19,7 @@ __host__ void SaveMatrixToFile(char* fileName, int* matrix, int width, int heigh
 	fclose(file);
 }
 
-/////Функция транспонирования матицы
+/////Р¤СѓРЅРєС†РёСЏ С‚СЂР°РЅСЃРїРѕРЅРёСЂРѕРІР°РЅРёСЏ РјР°С‚РёС†С‹
 __global__ void transpose(int* inputMatrix, int* outputMatrix, int width, int height) {
 	int x = blockDim.x * blockIdx.x + threadIdx.x;
 	int y = blockDim.y * blockIdx.y + threadIdx.y;
@@ -29,7 +29,7 @@ __global__ void transpose(int* inputMatrix, int* outputMatrix, int width, int he
 			outputMatrix[x * height + y] = inputMatrix[y * width + x];
 }
 
-/////Функция транспонирования матицы на хосте
+/////Р¤СѓРЅРєС†РёСЏ С‚СЂР°РЅСЃРїРѕРЅРёСЂРѕРІР°РЅРёСЏ РјР°С‚РёС†С‹ РЅР° С…РѕСЃС‚Рµ
 __host__ void transponse_on_host(int* inputMatrix, int* outputMatrix, int width, int height) {
 	for (int y = 0; y < width; y++) {
 		for (int x = 0; x < height; x++) {
@@ -40,63 +40,63 @@ __host__ void transponse_on_host(int* inputMatrix, int* outputMatrix, int width,
 
 __host__ int main() {
 	setlocale(LC_CTYPE, "rus");
-	int width;     //Ширина матрицы
-	int height;    //Высота матрицы
-	printf("Введите кол-во столбцов: ");
+	int width;     //РЁРёСЂРёРЅР° РјР°С‚СЂРёС†С‹
+	int height;    //Р’С‹СЃРѕС‚Р° РјР°С‚СЂРёС†С‹
+	printf("Р’РІРµРґРёС‚Рµ РєРѕР»-РІРѕ СЃС‚РѕР»Р±С†РѕРІ: ");
 	scanf("%d", &width);
-	printf("Введите кол-во строк: ");
+	printf("Р’РІРµРґРёС‚Рµ РєРѕР»-РІРѕ СЃС‚СЂРѕРє: ");
 	scanf("%d", &height);
 	int N = width*height;
 
-	//Выделение памяти под матрицы на хосте
-	int* A;  //Исходная матрица 
+	//Р’С‹РґРµР»РµРЅРёРµ РїР°РјСЏС‚Рё РїРѕРґ РјР°С‚СЂРёС†С‹ РЅР° С…РѕСЃС‚Рµ
+	int* A;  //РСЃС…РѕРґРЅР°СЏ РјР°С‚СЂРёС†Р° 
 	A = (int *)malloc(sizeof(int) * N);
-	int* A_t; //Транспонированная матрица
+	int* A_t; //РўСЂР°РЅСЃРїРѕРЅРёСЂРѕРІР°РЅРЅР°СЏ РјР°С‚СЂРёС†Р°
 	A_t = (int *)malloc(sizeof(int) * N);
-	int* A_t_host; //Транспонированная матрица на хосте
+	int* A_t_host; //РўСЂР°РЅСЃРїРѕРЅРёСЂРѕРІР°РЅРЅР°СЏ РјР°С‚СЂРёС†Р° РЅР° С…РѕСЃС‚Рµ
 	A_t_host = (int *)malloc(sizeof(int) * N);
 
-	//Заполняем матрицу данными
+	//Р—Р°РїРѕР»РЅСЏРµРј РјР°С‚СЂРёС†Сѓ РґР°РЅРЅС‹РјРё
 	for (int i = 0; i < N; i++) {
 		A[i] = i + 1;
 	}
 
 
-	//Записываем исходную матрицу в файл
+	//Р—Р°РїРёСЃС‹РІР°РµРј РёСЃС…РѕРґРЅСѓСЋ РјР°С‚СЂРёС†Сѓ РІ С„Р°Р№Р»
 	SaveMatrixToFile("matrix.txt", A, width, height);
 
-	//Выделение памяти под матрицы на девайсе
-	int* A_dev; //Исходная матрица 
-	int* A_t_dev; //Транспонированная матрица 
+	//Р’С‹РґРµР»РµРЅРёРµ РїР°РјСЏС‚Рё РїРѕРґ РјР°С‚СЂРёС†С‹ РЅР° РґРµРІР°Р№СЃРµ
+	int* A_dev; //РСЃС…РѕРґРЅР°СЏ РјР°С‚СЂРёС†Р° 
+	int* A_t_dev; //РўСЂР°РЅСЃРїРѕРЅРёСЂРѕРІР°РЅРЅР°СЏ РјР°С‚СЂРёС†Р° 
 
-				  //Выделяем глобальную память для храния данных на девайсе
+				  //Р’С‹РґРµР»СЏРµРј РіР»РѕР±Р°Р»СЊРЅСѓСЋ РїР°РјСЏС‚СЊ РґР»СЏ С…СЂР°РЅРёСЏ РґР°РЅРЅС‹С… РЅР° РґРµРІР°Р№СЃРµ
 	cudaMalloc((void**)&A_dev, N * sizeof(int));
 	cudaMalloc((void**)&A_t_dev, N * sizeof(int));
 
-	//Копируем исходную матрицу с хоста на девайс
+	//РљРѕРїРёСЂСѓРµРј РёСЃС…РѕРґРЅСѓСЋ РјР°С‚СЂРёС†Сѓ СЃ С…РѕСЃС‚Р° РЅР° РґРµРІР°Р№СЃ
 	cudaMemcpy(A_dev, A, N * sizeof(int), cudaMemcpyHostToDevice);
 
 
 	dim3 gridSize = dim3(width / 8, height / 8, 1);
 	dim3 blockSize = dim3(8, 8, 1);
 
-	//Начать отсчета времени
+	//РќР°С‡Р°С‚СЊ РѕС‚СЃС‡РµС‚Р° РІСЂРµРјРµРЅРё
 	srand(time(0));
 
-	//Запуск ядра 
+	//Р—Р°РїСѓСЃРє СЏРґСЂР° 
 	transpose << <gridSize, blockSize >> >(A_dev, A_t_dev, width, height);
 
-	//Окончание работы ядра, остановка времени 
+	//РћРєРѕРЅС‡Р°РЅРёРµ СЂР°Р±РѕС‚С‹ СЏРґСЂР°, РѕСЃС‚Р°РЅРѕРІРєР° РІСЂРµРјРµРЅРё 
 	clock_t device_time = clock();
 
 
-	//Копируем результат с девайса на хост
+	//РљРѕРїРёСЂСѓРµРј СЂРµР·СѓР»СЊС‚Р°С‚ СЃ РґРµРІР°Р№СЃР° РЅР° С…РѕСЃС‚
 	cudaMemcpy(A_t, A_t_dev, N * sizeof(int), cudaMemcpyDeviceToHost);
 
-	//Записываем транспонированную матрицу в файл
+	//Р—Р°РїРёСЃС‹РІР°РµРј С‚СЂР°РЅСЃРїРѕРЅРёСЂРѕРІР°РЅРЅСѓСЋ РјР°С‚СЂРёС†Сѓ РІ С„Р°Р№Р»
 	SaveMatrixToFile("transpose matrix.txt", A_t, height, width);
 
-	//Удаление ресурсов с видеокарты
+	//РЈРґР°Р»РµРЅРёРµ СЂРµСЃСѓСЂСЃРѕРІ СЃ РІРёРґРµРѕРєР°СЂС‚С‹
 	cudaFree(A_dev);
 	cudaFree(A_t_dev);
 
@@ -104,17 +104,17 @@ __host__ int main() {
 	srand(time(0));
 	transponse_on_host(A, A_t_host, width, height);
 
-	//Записываем транспонированную матрицу в файл
+	//Р—Р°РїРёСЃС‹РІР°РµРј С‚СЂР°РЅСЃРїРѕРЅРёСЂРѕРІР°РЅРЅСѓСЋ РјР°С‚СЂРёС†Сѓ РІ С„Р°Р№Р»
 	SaveMatrixToFile("transpose matrix on host.txt", A_t_host, height, width);
 	clock_t host_time = clock();
 
 
-	printf("Время работы на устройстве составило: %d мкс\n", device_time * 1000);
-	printf("Время работы на хосте составило: %d мкс\n", host_time * 1000);
-	printf("Выигрыш времени на устройстве составил: %d мкс\n", (host_time - device_time) * 1000);
+	printf("Р’СЂРµРјСЏ СЂР°Р±РѕС‚С‹ РЅР° СѓСЃС‚СЂРѕР№СЃС‚РІРµ СЃРѕСЃС‚Р°РІРёР»Рѕ: %d РјРєСЃ\n", device_time * 1000);
+	printf("Р’СЂРµРјСЏ СЂР°Р±РѕС‚С‹ РЅР° С…РѕСЃС‚Рµ СЃРѕСЃС‚Р°РІРёР»Рѕ: %d РјРєСЃ\n", host_time * 1000);
+	printf("Р’С‹РёРіСЂС‹С€ РІСЂРµРјРµРЅРё РЅР° СѓСЃС‚СЂРѕР№СЃС‚РІРµ СЃРѕСЃС‚Р°РІРёР»: %d РјРєСЃ\n", (host_time - device_time) * 1000);
 	getch();
 
-	//Удаление ресурсов с хоста
+	//РЈРґР°Р»РµРЅРёРµ СЂРµСЃСѓСЂСЃРѕРІ СЃ С…РѕСЃС‚Р°
 	delete[] A;
 	delete[] A_t;
 
